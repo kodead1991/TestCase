17f3d7422dda33213a0d33b027d5fec412aa9ee2
kodead1991 <kodead@mail.ru>
2021-07-06 21:53:23 +0300

    Initial commit

---
+++ TestCaseConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace TestCaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\SomeDir\hta.txt";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
