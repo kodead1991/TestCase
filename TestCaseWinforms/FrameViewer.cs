fb8e7aa5daddf1f1e8aff49cb45df2490e880426
kodead1991 <kodead@mail.ru>
2021-07-13 07:07:01 +0300

    asd

--- TestCaseWinforms/FrameViewer.cs
+++ TestCaseWinforms/FrameViewer.cs
@@ -10,10 +10,25 @@ using System.Windows.Forms;
 
 namespace TestCaseWinforms
 {
+    public class FrameViewInfo
+    {
+        private ushort mask;
+        private string structure;
+
+        public ushort Mask { get { return mask; } set { mask = value; } }
+        public string Structure { get { return structure; } set { structure = value; } }
+        public FrameViewInfo(ushort mask, string structure)
+        {
+            Mask = mask; Structure = structure;
+        }
+    }
     public partial class FrameViewer : UserControl
     {
-        public string[] drawString;
-        public Kadr k { get; set; }
+        public Frame drawFrame;
+        public FrameViewInfo frameInfo;
+        private string radix;
+
+        public string Radix { get { return radix; } set { radix = value; } }
 
         public FrameViewer()
         {
@@ -22,68 +37,68 @@ namespace TestCaseWinforms
         }
 
         //отображение кадра в Control'е в формате HEX
-        public void displayKadrHEX(int kadrNum)
-        {
-            if (k == null || k.kadr.Length == 0)
-                return;
+        //public void displayKadrHEX(int kadrNum)
+        //{
+        //    if (drawFrame == null || drawFrame.Length == 0)
+        //        return;
 
-            drawString = new string[k.wordCount];
+        //    drawString = new string[k.wordCount];
 
-            for (int i = 0; i < 31; i++)
-            {
-                drawString[i] = (Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x3FF).ToString("X4"); //дополняем нулями до 4-х знаков в HEX'e (0000 to FFFF)
-            }
-            for (int i = 31; i < k.wordCount; i++)
-            {
-                if (k.kadrType == "(0111111110) БИТС-М")
-                    drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1FE) >> 1).ToString("X2"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
-                else if (k.kadrType == "(0111001110) VITS-M")
-                    drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1CE) >> 1).ToString("X2"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
-            }
-            this.Invalidate();
-        }
+        //    for (int i = 0; i < 31; i++)
+        //    {
+        //        drawString[i] = (Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x3FF).ToString("X4"); //дополняем нулями до 4-х знаков в HEX'e (0000 to FFFF)
+        //    }
+        //    for (int i = 31; i < k.wordCount; i++)
+        //    {
+        //        if (k.kadrType == "(0111111110) БИТС-М")
+        //            drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1FE) >> 1).ToString("X2"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
+        //        else if (k.kadrType == "(0111001110) VITS-M")
+        //            drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1CE) >> 1).ToString("X2"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
+        //    }
+        //    this.Invalidate();
+        //}
 
-        //отображение кадра в Control'е в формате DEC
-        public void displayKadrDEC(int kadrNum)
-        {
-            if (k == null || k.kadr.Length == 0)
-                return;
+        ////отображение кадра в Control'е в формате DEC
+        //public void displayKadrDEC(int kadrNum)
+        //{
+        //    if (k == null || k.kadr.Length == 0)
+        //        return;
 
-            drawString = new string[k.wordCount];
+        //    drawString = new string[k.wordCount];
 
-            for (int i = 0; i < 31; i++)
-            {
-                drawString[i] = (Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x3FF).ToString("X4"); //дополняем нулями до 4-х знаков в HEX'e (0000 to FFFF)
-            }
-            for (int i = 31; i < k.wordCount; i++)
-            {
-                if (k.kadrType == "(0111111110) БИТС-М")
-                    drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1FE) >> 1).ToString("D3"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
-                else if (k.kadrType == "(0111001110) VITS-M")
-                    drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1CE) >> 1).ToString("D3"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
-            }
-            this.Invalidate();
-        }
+        //    for (int i = 0; i < 31; i++)
+        //    {
+        //        drawString[i] = (Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x3FF).ToString("X4"); //дополняем нулями до 4-х знаков в HEX'e (0000 to FFFF)
+        //    }
+        //    for (int i = 31; i < k.wordCount; i++)
+        //    {
+        //        if (k.kadrType == "(0111111110) БИТС-М")
+        //            drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1FE) >> 1).ToString("D3"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
+        //        else if (k.kadrType == "(0111001110) VITS-M")
+        //            drawString[i] = ((Convert.ToInt32(k.kadr[kadrNum, i], 16) & 0x1CE) >> 1).ToString("D3"); //дополняем нулями до 2-х знаков в HEX'e (00 to FF)
+        //    }
+        //    this.Invalidate();
+        //}
         private void FrameViewer_Paint(object sender, PaintEventArgs e)
         {
-            if (drawString == null || drawString.Length == 0)
-                return;
+            //if (drawString == null || drawString.Length == 0)
+            //    return;
 
-            //рисуем название и служебную часть кадра
-            Font drawFont = new Font("Courier New", 10);
-            SolidBrush drawBrush = new SolidBrush(Color.DarkMagenta);
+            ////рисуем название и служебную часть кадра
+            //Font drawFont = new Font("Courier New", 10);
+            //SolidBrush drawBrush = new SolidBrush(Color.DarkMagenta);
 
-            e.Graphics.DrawString("Служебная часть кадра", drawFont, drawBrush, 10, 10);
-            for (int j = 0; j < 31; j++)
-                e.Graphics.DrawString(drawString[j].ToString(), drawFont, drawBrush, 10 + j * 40, 30);
+            //e.Graphics.DrawString("Служебная часть кадра", drawFont, drawBrush, 10, 10);
+            //for (int j = 0; j < 31; j++)
+            //    e.Graphics.DrawString(drawString[j].ToString(), drawFont, drawBrush, 10 + j * 40, 30);
 
-            //рисуем название и информационную часть кадра
-            drawBrush = new SolidBrush(Color.DarkBlue);
-            e.Graphics.DrawString("Информационная часть кадра", drawFont, drawBrush, 10, 70);
-            for (int i = 0; i < k.wordCount - 31; i++)
-            {
-                e.Graphics.DrawString(drawString[31 + i].ToString(), drawFont, drawBrush, 10 + i % 32 * 40, 90 + i / 32 * 20);
-            }
+            ////рисуем название и информационную часть кадра
+            //drawBrush = new SolidBrush(Color.DarkBlue);
+            //e.Graphics.DrawString("Информационная часть кадра", drawFont, drawBrush, 10, 70);
+            //for (int i = 0; i < k.wordCount - 31; i++)
+            //{
+            //    e.Graphics.DrawString(drawString[31 + i].ToString(), drawFont, drawBrush, 10 + i % 32 * 40, 90 + i / 32 * 20);
+            //}
 
         }
     }
