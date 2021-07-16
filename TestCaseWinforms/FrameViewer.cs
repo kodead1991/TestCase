6e9cee965558ad23e811f8155c0ae68d4d440f79
kodead1991 <kodead@mail.ru>
2021-07-16 16:24:16 +0300

    Рамка сделана и выводится где надо

--- TestCaseWinforms/FrameViewer.cs
+++ TestCaseWinforms/FrameViewer.cs
@@ -14,7 +14,8 @@ namespace TestCaseWinforms
     {
         Frame _frameToShow;
         string _path;
-        int frameInfoSize = 31;
+        int wordsServiceCount = 31;
+        int frameRowNumber = 32;
         Font _drawFont = new Font("Courier New", 10);
         SolidBrush _drawBrushService = new SolidBrush(Color.DarkMagenta);
         SolidBrush _drawBrushData = new SolidBrush(Color.DarkBlue);
@@ -27,6 +28,7 @@ namespace TestCaseWinforms
         Point _offsetData = new Point(10, 90);
         Pen _myPen = new Pen(Brushes.DeepSkyBlue);
         Point _mousePos;
+        int posX, posY;
         //Rectangle _rectangle = new Rectangle(_offsetService.X - 1, _offsetService.Y + 15, _cellWidth, _cellHeight);
 
         public Frame FrameToShow
@@ -54,27 +56,28 @@ namespace TestCaseWinforms
             get { return _mousePos; }
             set
             {
+                //posX = value.X; posY = value.Y;
                 if (value.X >= _offsetService.X
-                    && value.X < _offsetService.X + _cellSize.Width * 32
-                    && value.Y >= _offsetService.Y + _cellSize.Height
-                    && value.Y < _offsetService.Y + _cellSize.Height + _cellSize.Height)
+                    && value.X < _offsetService.X + _cellSize.Width * wordsServiceCount
+                    && value.Y >= _offsetService.Y
+                    && value.Y < _offsetService.Y + _cellSize.Height)
                 {
-                    int posX = value.X / _cellSize.Width * _cellSize.Width + _offsetService.X;
-                    int posY = value.Y / _cellSize.Height * _cellSize.Height + +_offsetService.X;
+                    int posX = _offsetService.X - 3 + ((value.X - _offsetService.X) / _cellSize.Width) * _cellSize.Width;
+                    int posY = _offsetService.Y - 3 + ((value.Y - _offsetService.Y) / _cellSize.Height) * _cellSize.Height;
                     _mousePos = new Point(posX, posY);
                     Invalidate();
                 }
                 else if (value.X >= _offsetData.X
-                    && value.X < _offsetData.X + _cellSize.Width * 32
+                    && value.X < _offsetData.X + _cellSize.Width * frameRowNumber
                     && value.Y >= _offsetData.Y
-                    && value.Y < _offsetData.Y + _cellSize.Height * (_frameToShow.Length / 32 + 1))
+                    && value.Y < _offsetData.Y + _cellSize.Height * (_frameToShow.Length / frameRowNumber))
                 {
-                    int posX = value.X / _cellSize.Width * _cellSize.Width + +_offsetData.X;
-                    int posY = value.Y / _cellSize.Height * _cellSize.Height +_offsetData.X;
+                    int posX = _offsetService.X - 3 + ((value.X - _offsetService.X) / _cellSize.Width) * _cellSize.Width;
+                    int posY = _offsetService.Y - 3 + ((value.Y - _offsetService.Y) / _cellSize.Height) * _cellSize.Height;
                     _mousePos = new Point(posX, posY);
                     Invalidate();
                 }
-                
+
             }
         }
 
@@ -94,20 +97,20 @@ namespace TestCaseWinforms
                 return;
 
             //рисуем служебную часть кадра
-            e.Graphics.DrawString("Служебная часть кадра \t Путь файла: " + Path + " X:" + _mousePos.X + " Y:" + _mousePos.Y,
+            e.Graphics.DrawString("Служебная часть кадра \t Путь файла: " + Path + " XF:" + _mousePos.X + " YF:" + _mousePos.Y,
                 _drawFont,
                 _drawBrushService,
                 _offsetServiceStart.X,
                 _offsetServiceStart.Y);
 
-            for (int i = 0; i < frameInfoSize; i++)
+            for (int i = 0; i < wordsServiceCount; i++)
             {
                 e.Graphics.DrawString(FrameToShow.frameArray[i]
                     .ToString("X3"),
                     _drawFont,
                     _drawBrushService,
                     _offsetService.X + i * _cellSize.Width,
-                    _offsetService.Y + _cellSize.Height);
+                    _offsetService.Y);
             }
 
             //рисуем информационную часть кадра
@@ -117,11 +120,11 @@ namespace TestCaseWinforms
                 _offsetDataStart.X,
                 _offsetDataStart.Y);
 
-            for (int i = 0; i < FrameToShow.Length - frameInfoSize; i++)
+            for (int i = 0; i < FrameToShow.Length - wordsServiceCount; i++)
             {
-                var row = Math.DivRem(i, 32, out var column);
+                var row = Math.DivRem(i, frameRowNumber, out var column);
 
-                e.Graphics.DrawString((_param.GetWord(FrameToShow.frameArray[frameInfoSize + i]))
+                e.Graphics.DrawString((_param.GetWord(FrameToShow.frameArray[wordsServiceCount + i]))
                     .ToString(_radix),
                     _drawFont,
                     _drawBrushData,
